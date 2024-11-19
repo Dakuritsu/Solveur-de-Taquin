506405474f3b4ed4daa6243f01d89c423e6063cb
dakuritsu <74517832+Dakuritsu@users.noreply.github.com>
2024-11-19 01:28:53 +0100

    Création de Strategies.py et de la fonction bfs

---
+++ Strategies.py
@@ -0,0 +1,31 @@
+from collections import deque
+from heapq import heappop, heappush
+from typing import List, Tuple, Set
+
+class Strategies:
+    def __init__(self, taquin):
+        self.taquin = taquin
+
+    def bfs(self, avec_parcours=False) -> Tuple[List[List[int]], List[List[List[int]]]]:
+        """Résout le taquin avec l'algorithme BFS."""
+        queue = deque([(self.taquin.etat_initial, [])])
+        visites = set()
+        parcours = []
+
+        while queue:
+            etat, chemin = queue.popleft()
+            if avec_parcours:
+                parcours.append(etat)
+
+            if self.taquin.est_final(etat):
+                return chemin, parcours
+
+            etat_tuple = tuple(map(tuple, etat))
+            if etat_tuple in visites:
+                continue
+
+            visites.add(etat_tuple)
+            for suivant in self.taquin.generer_suivants(etat):
+                queue.append((suivant, chemin + [suivant]))
+
+        return None, parcours
