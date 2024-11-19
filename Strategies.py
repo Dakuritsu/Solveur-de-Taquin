8a6ffceab6de94cb414ea21d7f57ef3ccab1747e
dakuritsu <74517832+Dakuritsu@users.noreply.github.com>
2024-11-19 01:30:47 +0100

    Update Strategies.py
    Ajout de la fonction A*

--- Strategies.py
+++ Strategies.py
@@ -54,3 +54,38 @@ class Strategies:
 
         return None, parcours
     
+    def heuristique(self, etat: List[List[int]]) -> int:
+        """Calcule la distance de Manhattan pour une heuristique A*."""
+        taille = len(etat)
+        distance = 0
+        for i in range(taille):
+            for j in range(taille):
+                valeur = etat[i][j]
+                if valeur != 0:
+                    cible_x, cible_y = divmod(valeur - 1, taille)
+                    distance += abs(i - cible_x) + abs(j - cible_y)
+        return distance
+
+    def a_etoile(self, avec_parcours=False) -> Tuple[List[List[int]], List[List[List[int]]]]:
+        """Résout le taquin avec l'algorithme A*."""
+        queue = [(self.heuristique(self.taquin.etat_initial), 0, self.taquin.etat_initial, [])]
+        visites = set()
+        parcours = []
+
+        while queue:
+            _, cout, etat, chemin = heappop(queue)
+            if avec_parcours:
+                parcours.append(etat)
+
+            if self.taquin.est_final(etat):
+                return chemin, parcours
+
+            etat_tuple = tuple(map(tuple, etat))
+            if etat_tuple in visites:
+                continue
+
+            visites.add(etat_tuple)
+            for suivant in self.taquin.generer_suivants(etat):
+                heappush(queue, (cout + 1 + self.heuristique(suivant), cout + 1, suivant, chemin + [suivant]))
+
+        return None, parcours
