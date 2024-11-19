0a1ead3de0ace6a892d348d73cae2082572693d0
dakuritsu <74517832+Dakuritsu@users.noreply.github.com>
2024-11-19 01:30:04 +0100

    Update Strategies.py
    Ajout de la fonction dfs

--- Strategies.py
+++ Strategies.py
@@ -29,3 +29,28 @@ class Strategies:
                 queue.append((suivant, chemin + [suivant]))
 
         return None, parcours
+
+    def dfs(self, avec_parcours=False) -> Tuple[List[List[int]], List[List[List[int]]]]:
+        """Résout le taquin avec l'algorithme DFS."""
+        stack = [(self.taquin.etat_initial, [])]
+        visites = set()
+        parcours = []
+
+        while stack:
+            etat, chemin = stack.pop()
+            if avec_parcours:
+                parcours.append(etat)
+
+            if self.taquin.est_final(etat):
+                return chemin, parcours
+
+            etat_tuple = tuple(map(tuple, etat))
+            if etat_tuple in visites:
+                continue
+
+            visites.add(etat_tuple)
+            for suivant in self.taquin.generer_suivants(etat):
+                stack.append((suivant, chemin + [suivant]))
+
+        return None, parcours
+    
